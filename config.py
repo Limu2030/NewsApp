e52305ab7cdd577abdb68db4a6d628fd35bc8846
limu2030 <limu2030@gmail.com>
2022-05-06 22:59:53 +0300

    create instance config file

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 
 
 class Config:
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={497e7187eacd41839283d6145650e5df}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET KEY')
+    
 
 
 class ProdConfig(Config):
